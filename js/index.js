bcd7f7f36348deab04f8a21253d19a4a32dbbe18
Eduard-Konovka <ed098ua@gmail.com>
2022-06-07 15:51:27 +0300

    Optimizes screen orientation

--- js/index.js
+++ js/index.js
@@ -2,18 +2,14 @@
 import HorizontalGame from "./scenes/VerticalGame.js";
 import VerticalGame from "./scenes/VerticalGame.js";
 
-const breadth = window.innerWidth > window.innerHeight ? 1080 : 600;
-
-const screenOrientation =
-  window.innerWidth > window.innerHeight
-    ? window.innerHeight / 900
-    : window.innerWidth / 600;
-
 new Phaser.Game({
   type: Phaser.AUTO,
-  width: breadth,
+  width: window.innerWidth > window.innerHeight ? 1080 : 600,
   height: 900,
-  zoom: screenOrientation,
+  zoom:
+    window.innerWidth > window.innerHeight
+      ? window.innerHeight / 900
+      : window.innerWidth / 600,
   title: "My Fantasy: Make Your Story",
   // FIXME url
   url: "https://eduard-konovka.github.io/test-task-playable-ads/",
