23ac910f120369d23dbea92ce687319c86b45bff
Eduard-Konovka <ed098ua@gmail.com>
2022-06-07 03:04:08 +0300

    Updates physics

--- js/index.js
+++ js/index.js
@@ -4,5 +4,12 @@ new Phaser.Game({
   type: Phaser.AUTO,
   width: 600,
   height: 900,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
   scene: Game,
 });
