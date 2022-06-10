70dba4d6d0b8cbb89bb95baed4fa30419c816b45
Eduard-Konovka <ed098ua@gmail.com>
2022-06-11 00:59:59 +0300

    Fixes window.height

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 import GameHorizontal from "./scenes/GameHorizontal.js";
 import GameVertical from "./scenes/GameVertical.js";
 
+const body = document.querySelector("body");
+window.innerWidth < window.innerHeight &&
+  body.setAttribute("style", `height: 92vh`);
+
 new Phaser.Game({
   type: Phaser.AUTO,
   width: window.innerWidth > window.innerHeight ? 1080 : 600,
@@ -9,7 +13,6 @@ new Phaser.Game({
     window.innerWidth > window.innerHeight
       ? window.innerHeight / 900
       : window.innerWidth / 600,
-  autoCenter: 1,
   title: "My Fantasy: Make Your Story",
   url: "https://github.com/Eduard-Konovka/test-task-playable-ads",
   scene: window.innerWidth > window.innerHeight ? GameHorizontal : GameVertical,
