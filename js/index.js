6221b671c2dbd2eacdf6306b257f80a222a4a8e7
Eduard-Konovka <ed098ua@gmail.com>
2022-06-11 04:13:18 +0300

    Fixes index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import GameVertical from "./scenes/GameVertical.js";
 
 const body = document.querySelector("body");
 window.innerWidth < window.innerHeight &&
-  body.setAttribute("style", `height: 92vh`);
+  body.setAttribute("style", "height: 92vh");
 
 new Phaser.Game({
   type: Phaser.AUTO,
@@ -17,5 +17,3 @@ new Phaser.Game({
   url: "https://github.com/Eduard-Konovka/test-task-playable-ads",
   scene: window.innerWidth > window.innerHeight ? GameHorizontal : GameVertical,
 });
-
-// TODO choice of zoom
