97c5b2a62b8cd89ae102128438bce645202fe453
Eduard-Konovka <ed098ua@gmail.com>
2022-06-27 17:28:15 +0300

    Fixes the height of the mobile browser window

--- js/index.js
+++ js/index.js
@@ -4,13 +4,19 @@ const body = document.querySelector("body");
 window.innerWidth < window.innerHeight &&
   body.setAttribute("style", `height: ${window.innerHeight}px`);
 
+const mobileBrowserZoom =
+  navigator.userAgent.includes("Android") ||
+  navigator.userAgent.includes("iPhone")
+    ? (window.innerHeight / 900) * 1.19
+    : window.innerHeight / 900;
+
 new Phaser.Game({
   type: Phaser.AUTO,
   width: window.innerWidth > window.innerHeight ? 1080 : 600,
   height: 900,
   zoom:
     window.innerWidth > window.innerHeight
-      ? window.innerHeight / 900
+      ? mobileBrowserZoom
       : window.innerWidth / 600,
   title: "My Fantasy: Make Your Story",
   url: "https://github.com/Eduard-Konovka/test-task-playable-ads",
